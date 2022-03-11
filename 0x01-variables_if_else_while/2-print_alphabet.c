3bc0421f2c1bd2a8ee71cd5568db5b3a812fc7ce
SamuelNgundi <samuelwngundi02@gmail.com>
2022-03-11 20:28:37 +0300

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the last digit of the random
+ * number is stored in variable n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int x = 'A'; x <= 'Z' ; x++)
+	{
+		putchar(tolower(x));
+	}
+	return (0);
+}
