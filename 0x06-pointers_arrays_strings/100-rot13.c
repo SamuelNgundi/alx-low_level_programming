9cfd65a8b75288fb34151439417c45a874e12b69
SamuelNgundi <samuelwngundi02@gmail.com>
2022-03-23 23:16:07 +0300

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- * rot13 - encodes a string
- * @s: string
- * Return: s
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
  */
-char *rot13(char *s);
+char *rot13(char *s)
 {
 	int i, j;
 
