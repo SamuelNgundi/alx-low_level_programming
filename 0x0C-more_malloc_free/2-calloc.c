dc90bc463084bf5e5243d0af9907600ff6a6edb9
SamuelNgundi <samuelwngundi02@gmail.com>
2022-04-07 22:49:24 +0300

    Allocate memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,46 +1,28 @@
-#include <stdlib.h>
 #include "main.h"
-
+#include<stdlib.h>
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
-}
-
-/**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- *
- * Return: pointer to allocated memory
+ * _calloc - allocates memmory for array elements
+ * @nmemb: array indices
+ * @size: each size of indices
+ * Return: 0
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	char *strings;
+	unsigned int j;
 
 	if (nmemb == 0 || size == 0)
+	{
 		return (NULL);
-
-	ptr = malloc(size * nmemb);
-
-	if (ptr == NULL)
+	}
+	strings = malloc(nmemb * size);
+	if (strings == NULL)
+	{
 		return (NULL);
-
-	_memset(ptr, 0, nmemb * size);
-
-	return (ptr);
+	}
+	for (j = 0; j < (nmemb * size); j++)
+	{
+		strings[j] = '\0';
+	}
+	return (strings);
 }
