8e79258457b727f235d600df2048b72dd087a213
SamuelNgundi <samuelwngundi02@gmail.com>
2022-05-03 17:01:53 +0300

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,6 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
@@ -24,6 +25,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (t != s)
 			return (-1);
 	}
+
 	close(fd);
+
 	return (1);
 }
