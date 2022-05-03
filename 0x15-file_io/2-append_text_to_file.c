adc3706916f7773746f29fe47ec3e1760e17ba93
SamuelNgundi <samuelwngundi02@gmail.com>
2022-05-03 17:55:02 +0300

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,13 +2,14 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: The name of the file
- * @text_content: the NULL terminated string to add at the end of the file
- * Return: 1 on success, -1 on failure
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
+ *
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, s, t = 0;
+	int fd, t, s = 0;
 
 	if (!filename)
 		return (-1);
